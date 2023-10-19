a8b433452f1026fa2ededa495114b2017d089107
mikeelprogramador <128723321+mikeelprogramador@users.noreply.github.com>
2023-10-19 12:02:10 -0500

    version 1.08

--- 2023-10-19-a/despliegue.php
+++ 2023-10-19-a/despliegue.php
@@ -12,4 +12,22 @@ include("funciones.php");
 //echo visualizar();
 
 //para ver conexion 
-echo visualizar1();
+//echo visualizar1();
+
+//para ver conexion 
+//echo visualizar2();
+
+//para ver conexion 
+//echo contar_usuarios();
+
+//para ver conexion 
+//echo InsertarPersona("mario ramires","yyy");
+
+//para ver conexion 
+//echo EliminarPersona("mario ramires","yyy");
+
+//para ver conexion 
+//echo AptualizarSitios("mario ramires","yyy","https://www.youtube.com/results?search_query=como+descargar+minecraft+portable+para+pcla que ta");
+
+//para ver conexion 
+echo VisitarSitios("mario ramires","yyy","ven a mi cumpleaños");
--- 2023-10-19-a/funciones.php
+++ 2023-10-19-a/funciones.php
@@ -59,3 +59,161 @@ function visualizar1()
         $conexion->close(); //para terminar la conexion con la base de datos
     return $salida ;  //retorna 
 }
+
+function visualizar2()
+{
+    $salida = ""; //se inicializa la salida 
+
+    $conexion = mysqli_connect('localhost','root','','db_pruebas'); //conexion a la base de datos.
+
+    $sql  = "select 21 as edad"; // opera sql
+
+    $resultado = $conexion->query($sql);//ejecuta la consulta.
+    //recome el recorset 
+        while($fila = $resultado->fetch_assoc())//cilo mientras para que muestre en pantalla.
+        {
+            
+            if ($fila['edad'] >= 18 )
+            {
+                $salida.= "Tienes ".$fila['edad']." Eres un adulto muy adultoso";//los datos del sql al cual apodamos como "suma".
+                //puede incrementar o acoumular
+            }else{
+                $salida.="Tienes ".$fila['edad']." Eres un crio . :)";//los datos del sql al cual apodamos como "suma".
+                //puede incrementar o acoumular
+            }
+        }
+
+        $conexion->close(); //para terminar la conexion con la base de datos
+    return $salida ;  //retorna 
+}
+
+function contar_usuarios()
+{
+    $salida = ""; //se inicializa la salida 
+
+    $conexion = mysqli_connect('localhost','root','','db_pruebas'); //conexion a la base de datos.
+
+    $sql  = "select count(*) from personas"; // opera sql
+
+    $resultado = $conexion->query($sql);//ejecuta la consulta.
+    //recome el recorset 
+        while($fila = $resultado->fetch_array())//cilo mientras para que muestre en pantalla.
+        {
+            $salida = $fila[0];//los datos del sql al cual apodamos como "suma".
+            //puede incrementar o acoumular
+        }
+
+        $conexion->close(); //para terminar la conexion con la base de datos
+    return $salida ;  //retorna 
+}
+
+function InsertarPersona($nombre,$clave)
+{
+    $salida = ""; //se inicializa la salida 
+
+    $conexion = mysqli_connect('localhost','root','','db_pruebas'); //conexion a la base de datos.
+
+    $sql  = "insert into personas(nombre,clave) values('$nombre','$clave')"; // opera sql
+
+    $resultado = $conexion->query($sql);//ejecuta la consulta.
+
+        if($conexion->affected_rows > 0)//si a sucedio algun cambio en la base 
+        {
+            $salida .= "Te has ingresado correctamente :)";//mensaje si se registro de manera correcta
+        }else{
+            $salida .= "Error: 502";//mensaje en caso dee erro 
+        }
+
+        $conexion->close(); //para terminar la conexion con la base de datos
+    return $salida ;  //retorna 
+}
+
+function Encontarpersona($nombre,$clave)
+{
+    $salida = 0; //se inicializa la salida 
+
+    $conexion = mysqli_connect('localhost','root','','db_pruebas'); //conexion a la base de datos.
+
+    $sql  = "select id from personas where nombre='$nombre' and clave='$clave'"; // opera sql
+
+    $resultado = $conexion->query($sql);//ejecuta la consulta.
+
+        while($fila = $resultado->fetch_assoc())
+        {
+            $salida += $fila['id'] ;
+        }
+
+        $conexion->close(); //para terminar la conexion con la base de datos
+    return $salida ;  //retorna   
+}
+
+
+function EliminarPersona($nombre,$clave)
+{
+    $salida = ""; //se inicializa la salida 
+
+    $conexion = mysqli_connect('localhost','root','','db_pruebas'); //conexion a la base de datos.
+
+    $eliminar = Encontarpersona($nombre,$clave);
+    //echo $eliminar;
+
+    $sql  = "delete from personas where id='$eliminar'"; // opera sql
+
+    $resultado = $conexion->query($sql);//ejecuta la consulta.
+
+        if($conexion->affected_rows > 0)//si a sucedio algun cambio en la base 
+        {
+            $salida .= "Espero que regreses pronto :(";//mensaje si se elimina de manera correcta
+        }else{
+            $salida .= "Error: 502";//mensaje en caso dee erro 
+        }
+
+        $conexion->close(); //para terminar la conexion con la base de datos
+    return $salida ;  //retorna 
+}
+
+function AptualizarSitios($nombre,$clave,$sitio)
+{
+    $salida = ""; //se inicializa la salida 
+
+    $conexion = mysqli_connect('localhost','root','','db_pruebas'); //conexion a la base de datos.
+
+    $usuario = Encontarpersona($nombre,$clave);
+
+    $sql  = "update personas set sitio ='$sitio' where id='$usuario'"; // opera sql
+    //echo $sql;
+
+    $resultado = $conexion->query($sql);//ejecuta la consulta.
+
+        if($conexion->affected_rows > 0)//si a sucedio algun cambio en la base 
+        {
+            $salida .= "tu sitio se a cargado a:  "."$sitio";//mensaje si se elimina de manera correcta
+        }else{
+            $salida .= "Error: 502";//mensaje en caso dee erro 
+        }
+
+        $conexion->close(); //para terminar la conexion con la base de datos
+    return $salida ;  //retorna 
+}
+
+function VisitarSitios($nombre,$clave,$invitacion)
+{
+    $salida = ""; //se inicializa la salida 
+
+    $conexion = mysqli_connect('localhost','root','','db_pruebas'); //conexion a la base de datos.
+
+    $buscar = Encontarpersona($nombre,$clave);
+
+    $sql  = "select sitio from personas where id='$buscar'"; // opera sql
+ 
+    $resultado = $conexion->query($sql);//ejecuta la consulta.
+
+       while($fila =$resultado->fetch_assoc())
+       {
+        $salida.= "<a href='".$fila['sitio']."'>$invitacion<a/>";
+
+       }    
+
+        $conexion->close(); //para terminar la conexion con la base de datos
+    return $salida ;  //retorna 
+}
