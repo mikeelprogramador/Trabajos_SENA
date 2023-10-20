716a791af3f88977f9b779b51240f812780a20f6
mikeelprogramador <128723321+mikeelprogramador@users.noreply.github.com>
2023-10-20 12:19:10 -0500

    version 1.05 corregir

--- 2023-10-20-a/despliegue.php
+++ 2023-10-20-a/despliegue.php
@@ -3,6 +3,6 @@
 include("funciones.php");
 
 //funcion ConsultarPersona para mostra todas las personas de la base de datos 
-echo ConsultarPersona('','','','2');
+echo ConsultarPersona('','','','','nombre','clave','6');
 
  
--- 2023-10-20-a/funciones.php
+++ 2023-10-20-a/funciones.php
@@ -7,19 +7,34 @@
  * @param varchar password del usuario.
  * @param varchar un parametro para realizar el conteo, lo puede llenar con cualquier dato
  * @param int limita la tabla.
+ * @param varchar columna.
+ * @param varchar columna.
+ * @param int buscar el usuario
  * tenga en cuanta que el parametro tiene que esta para que se realice el conteo.
  */
-function ConsultarPersona($u = null,$c = null,$p = null,$b = null)
+function ConsultarPersona($u = null,$c = null,$p = null,$b = null,$a = null,$i = null,$f = null  )
 {
     $conexion = mysqli_connect('localhost','root','','db_pruebas');// conexion a la base de batos
     $salida = "";// inicialisar la variable de saliad 
-    $sql = "select * from personas limit $b ";// sql 
+    $sql = "select * from personas  ";// sql 
     if($u != null )  $sql .= "where id='$u' ";//para mostrar un usuario
     if($u != null and $c != null )  $sql .= " and clave='$c'";//para mostrar un usuario con contraseña
     if($p != null) $sql = "select count(*) as contar from personas";//nuevo sql para hacer el conteo
+    if($b != null) $sql = "select * from personas limit $b";//nuevo sql para hacer el limite
+    if($a != null) $sql = "select $a,$i from personas where id='$f'";//nuevo sql que haga lo que quiera 
     $resultado = $conexion->query($sql);//ejecucion del sql
     if($resultado->num_rows > 0)
     {
+
+        if($sql == "select $a,$i from personas where id='$f'")//para ver si el sql es igual $a
+        {
+            while($fila = $resultado->fetch_assoc())//ciclo para mostrar en pantalla
+            {
+                //muestra un conteo
+                $salida .= $fila["$a"]." ";
+                $salida .= $fila["$i"]." ";
+            }
+        }
         if($sql != "select count(*) as contar from personas")//cuando $sql sea diferente  de $p 
         // va ha mostar el codigo toda la tabla de la base de datos
         {
