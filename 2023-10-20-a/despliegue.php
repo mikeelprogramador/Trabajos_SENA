2131011d42780d90210c9460c36386d3ee5c26c2
mikeelprogramador <128723321+mikeelprogramador@users.noreply.github.com>
2023-10-20 08:02:23 -0500

    version 1.01

--- 2023-10-20-a/despliegue.php
+++ 2023-10-20-a/despliegue.php
@@ -4,3 +4,4 @@ include("funciones.php");
 
 //funcion para mostra todas las personas de la base de datos 
 echo ConsultarPersona();
+
--- 2023-10-20-a/funciones.php
+++ 2023-10-20-a/funciones.php
@@ -1,14 +1,20 @@
 <?php
-
-function ConsultarPersona()
+/**
+ * Esta fucnion ConsultarPersona de encarga de consultar y mostra las personas 
+ * de una base de datos.
+ * @return texto los datos de las personas de una base de datos.
+ * @param into un numero de identificar de la persona.
+ */
+function ConsultarPersona($u = null)
 {
     $conexion = mysqli_connect('localhost','root','','db_pruebas');// conexion a la base de batos
     $salida = "";// inicialisar la variable de saliad 
-    $sql = "select * from personas";// sql 
+    $sql = "select * from personas ";// sql 
+    if($u != null )  $sql .= "where id='$u'";//para mostrar un usuario
     $resultado = $conexion->query($sql);//ejecucion del sql
     while($fila = $resultado->fetch_assoc())//ciclo para mostrar en pantalla
     {
-        //nostra todos los campos de la base de datos 
+        //mostra todos los campos de la base de datos 
         $salida .= $fila['id']."  ";
         $salida .= $fila['nombre']."  ";
         $salida .= $fila['clave']."  ";
@@ -18,3 +24,4 @@ function ConsultarPersona()
     $conexion->close();//finalizar conexion 
     return $salida; //retornar salida 
 }
+
